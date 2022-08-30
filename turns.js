c0ff5ef1d25282f0f8852179a674540153fa52a3
Guillermo Seclen Alfaro <guille.2602@gmail.com>
2022-08-30 16:07:26 -0300

    Agregada función consultar turno

--- turns.js
+++ turns.js
@@ -1,12 +1,45 @@
-//Función para crear un objeto persona
+//Clase para crear un objeto persona con los datos del turno.
 
-function Person (name, phone, topics, info, type) {
-    this.name = name;
-    this.phone = phone;
-    this.topics = topics;
-    this.info = info;
-    this.type = type;
+class Person {
+    constructor (name, phone, topics, info, type, active, turn, date) {
+        this.name = name;
+        this.phone = phone;
+        this.topics = topics;
+        this.info = info;
+        this.type = type;
+        this.active = active;
+        this.turn = turn; 
+        this.date = date;
+    }
+    
+    cancelTurn () {
+        this.active = false;
+    }
+
+    assignTurnId (id) {
+        this.turn = id;
+    }
+
+    modifyTurn (date) {
+        this.date = date;
+    }
 }
+
+// Creo una función para solicitar una fecha para el turno
+
+    function askForDate(){
+        let day = parseInt(prompt('Ingrese un día para el turno'));
+        let month = parseInt(prompt('Ingrese el mes en formato numérico'));
+        let year = new Date();
+        year = year.getFullYear();
+        return new Date(year, month, day);
+    }
+
+//Creo un turno de prueba y una lista de turnos
+
+let julio = new Person ('Julio' , '1133969444' , 2 , ['Impuestos', 'Sueldos'], 2, true, 1, new Date( 2022,08,31,14,30))
+let turnos = [];
+turnos.push(julio);
 
 //Función para asignar un turno a una persona
 
@@ -19,19 +52,19 @@ function requestTurn() {
     let type = 0;
     let trueOrFalse = true;
 
-// Consulto tipo de contribuyente para saber la duración del turno 
-// Duración: 1h para R.I o sociedad si tiene menos de 3 temas, 2hs si son más de 3 temas / 30 min. para monotributistas.
+    // Consulto tipo de contribuyente para saber la duración del turno 
  
     do {
-
         type = parseInt(prompt('Ingrese "1" si es responsable inscripto o sociedad, "2" si es monotributista'));
         if (type == 1) {
+        // Duración de turnos para responsables insriptos o sociedades.
             if (cantTopics <= 3) {
                 alert('Su turno tendrá una duración máxima de una hora');
                 } else {
                     alert('Su turno tendra una duración máxima de 2 horas');
                 }
             trueOrFalse = false;
+        //Duración de turnos para monotributistas.
         } else if (type == 2) {
             alert('Su turno tendrá una duración máxima de media hora');
             trueOrFalse = false;
@@ -41,25 +74,48 @@ function requestTurn() {
             }
     } while (trueOrFalse);
     
-// Asigno en un vector los temas de la reunión.
+    // Asigno en un vector los temas de la reunión.
 
     for (i = 1; i <= cantTopics; i++){
         meetingTopics.push(prompt(`Ingrese en pocas palabras el tema ${i} de la reunión`));
     }  
     console.log(meetingTopics);
 
-    const persona = new Person (name, tel, cantTopics, meetingTopics, type);
+    // Solicito un día para la reunión
+
+    let dateImput = askForDate();
+
+    // Creo el objeto persona con los datos obtenidos.
+
+    let persona = new Person (name, tel, cantTopics, meetingTopics, type, true, 1, dateImput);
     console.log(persona);
-    alert(name + ', tu turno se ha generado correctamente, se coordinará telefónicamente al número ' + tel + ' el día de la reunión \n¡Muchas gracias!')
-
-    //Devuelve un objeto persona con la información del turno.
+    alert(name + ', tu turno se ha generado correctamente, se coordinará telefónicamente al número ' + tel + 
+    ' el horario de la reunión \n¡Muchas gracias!')
     return persona;
-    }
+}
 
-//Función provisoria, hay que reemplazarlo por uno que reciba una lista de turnos y agregue otro.
-function addTurn() {
-    let turnsList = [];
+//Función que agrega el turno al array de turnos.
+
+function addTurn(turnsList) {
     turnsList.push(requestTurn());
+    const turnID = turnsList.length;
+    turnsList[turnsList.length - 1].assignTurnId(turnID);
     console.log(turnsList);
+    alert(`Su número de turno es ${turnID}`);
     return turnsList;
+}
+
+//Busca un turno por número de turno.
+
+function findTurn(turnNro, turnsList) {
+    const found = turnsList.find((turno) => turno.turn == turnNro);
+    return found;
+}
+
+// Consultar un turno
+
+function findTurnForUser(turnsList) {
+    const id = parseInt(prompt('Ingrese el número de turno a buscar'));
+    const turn = findTurn(id, turnsList);
+    alert(`Usted tiene turno para el día ${turn.date.getDate()}/${turn.date.getMonth()}/${turn.date.getFullYear()} por los siguientes temas: ${turn.info.toString()}`)
 }
